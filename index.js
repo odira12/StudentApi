f88d07695f2f14561998df04806efde7ed1c0883
odira <odiraelizabeth12@gmail.com>
2024-03-18 23:27:50 +0300

    StudentApi

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const studentRoute = require("./Routes/StudentRouter");
+const course =require("./Routes");
+
 require("dotenv").config();
 require("./model/dbConnect");
 
@@ -8,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use("/api/student", studentRoute);
+app.user("/api/course", courseRoute);
 
 app.listen(process.env.port || 4000, function() {
     console.log("Now listening for requests on: http://localhost:4000");
