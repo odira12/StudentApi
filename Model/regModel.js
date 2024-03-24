6d00059a9a5dd78fa1e4ba6830a90ff4336207f4
odira <odiraelizabeth12@gmail.com>
2024-03-24 16:58:53 +0300

    hashed password

--- Model/regModel.js
+++ Model/regModel.js
@@ -1,29 +1,46 @@
+const bcrypt = require("bcrypt");
+
 module.exports = (sequelize, DataTypes) => {
     const Reg = sequelize.define('registration', {
-
         reg_id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true,
         },
-
         regName: {
             type: DataTypes.STRING,
             allowNull: false,
         },
-
         regEmail: {
             type: DataTypes.STRING,
             allowNull: false,
         },
-        
         regPassword: {
             type: DataTypes.STRING,
             allowNull: false,
         },
     });
 
-    // Function to hash password berore saving
+    
+    // Function to hash password before saving
+    Reg.beforeCreate( async (user) => {
+        try {
+            const salt = await bcrypt.genSalt(12);
+            const hashedPwd = await bcrypt.hash(user.regPassword, salt)
+            user.regPassword = hashedPwd;
+        } catch (error) {
+            throw new Error("Error encrypting password")
+        }
+    })
 
+    // Function to compare the entered password with the saved hashed password
+    Reg.prototype.isValidPassword = async function(password) {
+        try {
+            return await bcrypt.compare(password, this.password);
+        } catch (error) {
+            throw error;
+        }
+    };
+    
     return Reg;
 }
